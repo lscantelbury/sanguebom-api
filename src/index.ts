793a1c6aa6ee81e2d3522b77f1aa9a63d8d47ae7
Arthur Santos <aldsb.snf20@uea.edu.br>
2022-09-17 23:00:57 -0400

    fix: fixing start command in package.json

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import express from "express";
 
+import "dotenv/config";
+
 import cors from "cors";
 import { routes } from "./routes";
 
@@ -9,4 +11,4 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333, () => console.log("Server is running!"));
+app.listen(process.env.PORT || 3333, () => console.log("Server is running!"));
